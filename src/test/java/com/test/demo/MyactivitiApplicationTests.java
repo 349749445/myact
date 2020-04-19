482f094c34798dbdc157b026e65c5ae79d42b9a7
Your Name <you@example.com>
2020-04-19 17:06:49 +0800

    activiti

---
+++ src/test/java/com/test/demo/MyactivitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyactivitiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
